ec59310a238e70ab3fdeb4626af92236477aa0f4
nitishsaini706 <nitishsaini706@gmail.com>
2022-04-27 12:59:17 +0530

    create edit and method overrided post

--- backend/REST api/index.js
+++ backend/REST api/index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var methodOverride = require('method-override')
+const { v4: uuid } = require('uuid');
+uuid();
+app.use(methodOverride('_method'))
+
 
 const comments = [
 
     {
+        id:uuid(),
         username: ' todd',
         comment: 'this is load'
     },
     {
+        id:uuid(),
         username : 'khota',
         comment : 'this is khota'
     },
     {
+        id:uuid(),
         username : 'udem',
         comment : ' no electricity , sucks'
     }
@@ -40,11 +48,40 @@ app.get('/comments/new' ,(req,res)=> {
 
 //usig post method 
 app.post('/comments' , (req,res)=>{
-    const { username, comment } = req.body;
-    comments.push({ username, comment});
+    const { urn, cmt ,id } = req.body;
+    comments.push({ urn, cmt , id:uuid()});
     res.redirect("/comments");
 })
+
+// getting comment from id 
+app.get('/comments/:id' , (req,res) => {
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/show' , {comment});
+})
+
+//editing comment 
+///we cannot use patch normally through http method using form 
+// so we use express override method , it trick browser in beliveing we are sending 
+// get or post requerst but in real we're sending patch
+
+app.patch('/comments/:id' , (req,res)=>{
+    const {id} = req.params;
+    const newComment = req.body.edit;
+    const foundComment = comments.find( c => c.id === id);
+    foundComment.comment = newComment;
+    res.redirect('/comments');
+})
+
+app.get("/comments/:id/edit" , (req,res)=>{
+    const {id} = req.params
+    const cmt = comments.find(c => c.id === id);
+    res.render('comments/edit' , {cmt});
+})
+
+
+
 app.listen(3000, (req, res) => {
-    console.log('liestening');
+    console.log('listening');
 })
 
