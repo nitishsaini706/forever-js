2ece1757797151e9456642f13e1ab5236412e758
nitishsaini706 <nitishsaini706@gmail.com>
2022-04-27 11:03:05 +0530

    added get and post method in form

--- backend/REST api/index.js
+++ backend/REST api/index.js
@@ -1,7 +1,23 @@
-const { urlencoded } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const comments = [
+
+    {
+        username: ' todd',
+        comment: 'this is load'
+    },
+    {
+        username : 'khota',
+        comment : 'this is khota'
+    },
+    {
+        username : 'udem',
+        comment : ' no electricity , sucks'
+    }
+]
+
 
 //to accept form data from url 
 app.use(express.urlencoded({ extended: true }))
@@ -9,11 +25,26 @@ app.use(express.urlencoded({ extended: true }))
 // to accept form data in json format from server
 app.use(express.json());
 
-app.set('views', path.join(__dirname__, 'views'));
-app.set('views engine', ejs);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.get('/comments' , (req,res) => 
+{
+    res.render('comments/index' , {comments});
+})
 
+// create new comment 
+app.get('/comments/new' ,(req,res)=> {
+    res.render('comments/new');
+})
 
+//usig post method 
+app.post('/comments' , (req,res)=>{
+    const { username, comment } = req.body;
+    comments.push({ username, comment});
+    res.redirect("/comments");
+})
 app.listen(3000, (req, res) => {
     console.log('liestening');
 })
+
