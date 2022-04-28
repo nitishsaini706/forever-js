525e717e18bb0d4d3caae75560647607a7a4de8f
nitishsaini706 <nitishsaini706@gmail.com>
2022-04-28 09:16:10 +0530

    learning virtual functions in mongoose

--- backend/database/person.js
+++ backend/database/person.js
@@ -6,3 +6,19 @@ async function main() {
   await mongoose.connect('mongodb://localhost:27017/movieApp');
   console.log('working');
 }
+
+const personSchema = new mongoose.Schema({
+    first:String,
+    last:String
+})
+
+//function present in mongoose but not in db
+personSchema.virtual('fulName').get(function(){
+    return `${this.first} ${this.last}`
+})
+
+const person = mongoose.model('person',personSchema);
+const nitish = new person({first:"nitish",last:"saini"});
+nitish.save();
+console.log(nitish.fulName);
+console.log(nitish);
