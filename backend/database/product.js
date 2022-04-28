90ad4b864bfb77d5da4d223bc845153716b3d958
nitishsaini706 <nitishsaini706@gmail.com>
2022-04-28 09:08:21 +0530

    learning functions in db

--- backend/database/product.js
+++ backend/database/product.js
@@ -12,20 +12,47 @@ const productSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    price:Number 
+    price:Number ,
+    onSale : {
+        type:Boolean,
+        default:false
+    }
 });
 
+// this work on instance of the db
+productSchema.methods.toggleOnSale = function()
+{
+    this.onSale= !this.onSale;
+    return this.save();
+}
+
+//this works on whole model 
+
+productSchema.statics.fireSale = function() {
+    return this.updateMany ({}, { onSale:true , price:0})
+}
 
 const product = mongoose.model('product',productSchema);
 
 const bike = new product({name:'yo bike',price:65000});
 
 bike.save()
-.then(data =>{
-    console.log("WRoked");
-    console.log(data);
-})
-.catch(err =>{
-    console.log("error ");
-    console.log(err);
-})
+// .then(data =>{
+//     console.log("WRoked");
+//     console.log(data);
+// })
+// .catch(err =>{
+//     console.log("error ");
+//     console.log(err);
+// })
+
+// const findProduct = async () => {
+//     const foundProduct = await product.findOne({name : 'yo bike'});
+//     console.log(foundProduct);
+//     await foundProduct.toggleOnSale();
+//     console.log(foundProduct);
+// }
+
+product.fireSale().then(data => console.log(data));
+
+// findProduct();
