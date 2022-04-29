1c7a3a34e5972f82c4b882653cc47f2a1bf2141b
nitishsaini706 <nitishsaini706@gmail.com>
2022-04-29 11:01:19 +0530

    added add new

--- yelp camp project/app.js
+++ yelp camp project/app.js
@@ -21,7 +21,7 @@ async function main() {
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -32,12 +32,22 @@ app.get('/campground', async(req,res)=>{
     res.render('campground/index',{camp});
 })
 
+app.get('/camground/new' , (req,res)=>{
+    res.render('campground/new');
+})
+
 app.get('/campground/:id' , async(req,res)=>{
     const {id} = req.params;
     const camp = await campground.findOne({id});
     res.render('campground/show',{camp});
 })
 
+app.post('/campground' , async(req,res)=>{
+    
+    const Campground = new campground(req.body.campground);
+    await Campground.save();
+    res.redirect(`/campground`);
+})
 
 app.listen(3000,()=>{
     console.log("server running on port 3000");
