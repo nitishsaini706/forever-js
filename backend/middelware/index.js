f543456eb8d05c0543398840624284a28bf63d84
nitishsaini706 <nitishsaini706@gmail.com>
2022-05-02 08:36:41 +0530

    first middleware

--- backend/middelware/index.js
+++ backend/middelware/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+app.use(morgan('tiny'));
+//how to use middleware 
+app.use((req,res,next)=>{
+    console.log("MIddleware");
+    next();
+})
+
+app.get('/',(req,res)=>{
+    res.send('Home page');
+})
+
+app.get('/dog',(req,res)=>{
+    res.send("WOOF WOOF !!!!");
+})
 
 app.listen(3000,()=>{
     console.log("App running on port 3000");
