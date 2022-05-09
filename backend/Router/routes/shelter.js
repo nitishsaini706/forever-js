9ed1c61a3bba4c257557fae68c8758b773c8750f
nitishsaini706 <nitishsaini706@gmail.com>
2022-05-09 09:15:33 +0530

    learning router 2

--- backend/Router/routes/shelter.js
+++ backend/Router/routes/shelter.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+// router.use((req,res,next)=>{
+//     next();
+// })
+
 router.get('/',(req,res)=>{
     res.send("shelters for the animals");
 })
 
-router.get('/:id',(req,res)=>{
+router.get('/:id',(req,res,next)=>{
     res.send("Editing one shelter");
 })
+
+module.exports = router;
