4fc5c2a4a2f8b1504945f03599bd46f142a1d46b
nitishsaini706 <nitishsaini706@gmail.com>
2022-05-09 10:01:21 +0530

    signed cookie

--- backend/Router/app.js
+++ backend/Router/app.js
@@ -7,13 +7,26 @@ const adminRouter = require('./routes/admin');
 
 // app.use('/shelter',shelterRouter);
 // app.use('/admin',adminRouter);
-app.use(cookies());
+// app.use(cookies());
+app.use(cookies('thisismysecret'));
 
 //fetching cookies 
 app.get('/greet',(req,res)=>{
     const {name} = req.cookies;
     res.send(`Hey there , ${name}`)
 })
+
+//signed cookies 
+app.get('/verify',(req,res)=>{
+    res.cookie('name','bhangu' ,{signed:true});
+    res.send('signe cookie');
+})
+
+app.get('/verifiedname',(req,res)=>{
+    // const {name} = req.signedCookies;
+    res.send(req.signedCookies);
+})
+
 //creating cookies
 
 app.get('/name',(req,res)=>{
