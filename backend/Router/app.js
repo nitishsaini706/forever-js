0a86a85770d9a01e3fc58d8183d690ac739a4534
nitishsaini706 <nitishsaini706@gmail.com>
2022-05-09 09:45:23 +0530

    using cookies

--- backend/Router/app.js
+++ backend/Router/app.js
@@ -4,8 +4,15 @@ const shelterRouter = require('./routes/shelter');
 const adminRouter = require('./routes/admin');
 
 
-app.use('/shelter',shelterRouter);
-app.use('/admin',adminRouter);
+// app.use('/shelter',shelterRouter);
+// app.use('/admin',adminRouter);
+
+//using cookies
+
+app.get('/name',(req,res)=>{
+    res.cookie('name','nitish');
+    res.send('Sent you a cookie');
+})
 
 app.listen(3000,(req,res)=>{
     console.log('APp running on port 3000');
