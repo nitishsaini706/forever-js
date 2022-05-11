9cade8e8e4fb245c2e40e99f14e8a478b53e6f32
nitishsaini706 <nitishsaini706@gmail.com>
2022-05-11 11:50:12 +0530

    added session for user

--- backend/auth/index.js
+++ backend/auth/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const User = require('./model/user');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
-const { mainModule } = require('process');
+const session = require('express-session');
 
 main().catch(e => console.log(e));
 
@@ -17,6 +17,7 @@ app.set('view engine','ejs');
 app.set('views','views');
 
 app.use(express.urlencoded({extended:true}));
+app.use(session({secret:'secret'}));
 
 app.get('/',(req,res)=>{
     res.send("working");
@@ -25,7 +26,6 @@ app.get('/',(req,res)=>{
 
 app.get('/register',(req,res)=>{
     res.render("register");
-    // res.send("register");
 })
 
 app.post('/register', async (req,res)=>{
@@ -33,9 +33,10 @@ app.post('/register', async (req,res)=>{
     const hash  = await bcrypt.hash(password , 12);
     const user = new User({username :username , password:hash});
     await user.save();
-    
+    req.session.user_id = user.id;
     res.redirect('/login');
 })
+
 app.get('/login',(req,res)=>{
     res.render('login');
 })
@@ -47,13 +48,22 @@ app.post('/login',async (req,res)=>{
     const pass = bcrypt.compare(password , user.password);
     if(pass)
     {
-        res.send("Welcome , YAY!!!!");
+        req.session.user_id = user._id;
+        res.redirect("/secret");
     }
     else{
         res.send("INvalid username or password");
     }
 
-    // res.send("checking");
+})
+
+app.get('/secret',(req,res)=>{
+    if(!req.session.user_id)
+    {
+        res.send('ONly happen when logged in');
+
+    }
+    res.redirect('/login');
 })
 
 app.listen(3000,()=>{
