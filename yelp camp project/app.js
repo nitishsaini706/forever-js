c180001c78c210f699c498902188f492ed7eaa22
nitishsaini706 <nitishsaini706@gmail.com>
2022-05-15 17:45:15 +0530

    fixing error related to redirection

--- yelp camp project/app.js
+++ yelp camp project/app.js
@@ -57,6 +57,7 @@ passport.serializeUser(user.serializeUser());
 passport.deserializeUser(user.deserializeUser());
 
 app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
--- yelp camp project/routes/user.js
+++ yelp camp project/routes/user.js
@@ -8,13 +8,20 @@ router.get('/register' ,(req,res)=>{
     res.render('user/register');
 })
 
-router.post('/register' , async(req,res) =>{
+router.post('/register' , async(req,res,next) =>{
     try{
         const {username , email ,password} = req.body;
         const user = new User({username , email});
         const registered = await User.register(user,password);
-        req.flash('success',"successfully registered");
-        res.redirect('/campground');
+        req.login(registered,err => {
+            if(err) 
+            {
+                return next(err);
+            }
+            req.flash('success',"successfully registered");
+            res.redirect('/campground');
+
+        })
     }catch(e)
     {
         req.flash('error',e.message);
@@ -28,7 +35,10 @@ router.get('/login' , (req,res)=>{
 
 router.post('/login' , Passport.authenticate('local', {failureFlash:true , failureRedirect:'/login'} ) ,(req,res) =>{
     req.flash('success',"Welcome back!!!!");
-    res.redirect('/campground');
+
+    const redirectUrl = req.session.returnTo || "/campground";
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 })
 
 module.exports = router;
